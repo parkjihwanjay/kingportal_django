f278e45c7cbe6a0505b7b509a394ea9ccdb405aa
jihwan <jihwan@jihwanui-MacBookPro.local>
2020-05-10 03:33:22 +0900

    login 삭제

--- kingportal/doubleMajor/urls.py
+++ kingportal/doubleMajor/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.Login, name='login'),
     path('apply/', views.Apply, name='apply'),
     path('info/', views.getInfo, name='test',)
 ]
